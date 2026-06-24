e4b0f957e14686d8e3f3e25f72f54499c56d5a31
Mylagoz <leonardomns96@gmail.com>
2026-06-24 14:56:05 -0400

    J-23

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  root: resolve(__dirname),
+  base: './',
+  server: {
+    port: 5173,
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '~utils': resolve(__dirname, 'utils')
+    }
+  },
   build: {
+    sourcemap: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
